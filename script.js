b187dfdefc7759785fb51290324e1e19dc6ab166
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-14 02:08:03 -0400

    modal open/close functionality

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const modal = document.getElementById('modal');
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav');
 });
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', (e) =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
